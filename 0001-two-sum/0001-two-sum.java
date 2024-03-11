0e6fcaae55e8362ab658ef936b2581d2e40d0ffd
Giri Prasath <120491771+GiriPrasath017@users.noreply.github.com>
2024-03-12 00:22:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len=nums.length-1;
+        for (int i=0;i<len;i++){
+            for (int j=i+1;j<=len;j++){
+                if((nums[i]+nums[j])==target){
+                    return new int[]{i,j};
+                }
